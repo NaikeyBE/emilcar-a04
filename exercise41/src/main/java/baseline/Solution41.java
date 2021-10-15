795e86d9e06251f2edb5283fff9a5bf50a6109c1
Naikey Emilcar <naikeybe@Knights.ucf.edu>
2021-10-15 12:57:13 -0400

    pseudocode and methods

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,59 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Naikey Emilcar
+ */
+
+package baseline;
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.lang.IllegalStateException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.NoSuchElementException;
+import java.lang.String;
+import java.lang.StringBuilder;
+
+public class Solution41 {
+
+    /*Create a program that reads in the following list of names
+     from a file called `exercise41_input.txt`
+      and sorts the list alphabetically:
+     */
+
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //Call file reader method
+
+        //Call formatting method
+
+        //Call print method
+    }
+
+    //read from file and stores data in string
+    private static String FileReader(){
+
+        String NameList = "";
+
+        return NameList;
+    }
+
+    //sorts data in string
+    public static String FormatArray(String List){
+
+        String str = "";
+
+        return str;
+
+    }
+
+    //prints out formatted data
+    public static void OutPrint(String List){
+
+
+    }
+
+}
---
+++ exercise41/src/test/java/TestCases.java
@@ -0,0 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Naikey Emilcar
+ */
+
+
+public class TestCases {
+}
